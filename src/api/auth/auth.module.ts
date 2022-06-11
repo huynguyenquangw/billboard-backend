66c1d48ecbe56c6e86237100706d7c53f3b3ca84
witm <huynguyenquangw@gmail.com>
2022-06-11 13:24:07 +0700

    Update auth module

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+// import { FacebookStrategy } from './facebook/facebook.strategy';
+import { AuthController } from './auth.controller';
 
-@Module({})
+@Module({
+  //   providers: [FacebookStrategy],
+  controllers: [AuthController],
+})
 export class AuthModule {}
