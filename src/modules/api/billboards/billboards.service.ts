6399d9823f9791462ffb3149ff2ce72b9b44523b
s3681437 <40589362+s3681437@users.noreply.github.com>
2022-07-05 21:02:56 +0700

    update search
    update search

--- src/modules/api/billboards/billboards.service.ts
+++ src/modules/api/billboards/billboards.service.ts
@@ -32,10 +32,15 @@ export class BillboardsService {
     return newBillboard.toDto();
   }
 
-  //Search and get all billboard by address2
-  async getAllbyAddress2(selectedAdrress2: string): Promise<Billboard[]> {
+  //Search and get all billboard by address2, rentalPrice, size_x, size_y, district(not done)
+  async search(selectedAdrress2: string, selectedPrice: number, selectedSize_x : number, selectedSize_y: number): Promise<Billboard[]> {
     return this.billboardRepository.find({
-      where: { address2: selectedAdrress2 },
+      where: {
+         address2: selectedAdrress2,
+         rentalPrice: selectedPrice,
+         size_x: selectedSize_x,
+         size_y: selectedSize_y,
+         },
       withDeleted: true,
     });
   }
--- src/modules/api/billboards/dto/create-billboard.dto.ts
+++ src/modules/api/billboards/dto/create-billboard.dto.ts
@@ -3,6 +3,7 @@ import {
   IsArray,
   IsNotEmpty,
   IsNumber,
+  IsOptional,
   IsString,
   IsUUID,
 } from 'class-validator';
@@ -15,42 +16,54 @@ export class CreateBillboardDto {
 
   @ApiProperty()
   @IsUUID()
-  @IsNotEmpty()
+  @IsOptional()
   readonly wardId: string;
 
   @IsString()
+  @IsOptional()
   address: string;
 
   @IsString()
+  @IsOptional()
   address2: string;
 
   @IsString()
+  @IsNotEmpty()
   name: string;
 
   @IsArray()
+  @IsOptional()
   picture: object[];
 
   @IsString()
+  @IsOptional()
   video: string;
 
   @IsNumber()
+  @IsOptional()
   size_x: number;
 
   @IsNumber()
+  @IsOptional()
   size_y: number;
 
   @IsNumber()
+  @IsOptional()
   circulation: number;
 
   @IsString()
+  @IsOptional()
   previousClient: string;
 
   @IsNumber()
+  @IsOptional()
   rentalPrice: number;
 
   @IsString()
+  @IsOptional()
   rentalDuration: string;
 
   @IsString()
+  @IsOptional()
   description: string;
 }
