971cd03b96df9c85c685203d8ddb261911b0bbc7
witm <huynguyenquangw@gmail.com>
2022-08-03 17:26:22 +0700

    Update filter mode in page options for billboard and user

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,5 @@ export * from './order-type';
 export * from './role-type';
 export * from './status-type';
 export * from './token-type';
+export * from './billboard-filter-mode';
+export * from './user-filter-mode';
