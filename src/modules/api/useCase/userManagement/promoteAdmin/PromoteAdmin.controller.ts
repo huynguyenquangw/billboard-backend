8a089c596e473f4222959e01b37e625e5b9308d1
witm <huynguyenquangw@gmail.com>
2022-09-05 01:25:47 +0700

    Update service for promotion

--- src/modules/api/useCase/userManagement/promoteAdmin/PromoteAdmin.controller.ts
+++ src/modules/api/useCase/userManagement/promoteAdmin/PromoteAdmin.controller.ts
@@ -1,12 +1,14 @@
 import {
   Body,
   Controller,
-  Get,
   HttpCode,
   HttpStatus,
   Param,
+  Post,
+  UseInterceptors,
 } from '@nestjs/common';
 import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { TransformInterceptor } from 'src/interceptors/TransformInterceptor.service';
 import { User } from 'src/modules/api/users/user.entity';
 import { PromoteAdminUseCase } from '.';
 
@@ -15,8 +17,8 @@ import { PromoteAdminUseCase } from '.';
 export class PromoteAdminController {
   constructor(private readonly useCase: PromoteAdminUseCase) {} // public readonly useCase: GetAllUserUseCase
 
-  @Get(':id')
-  @ApiOperation({ summary: 'Get 1 user by id' })
+  @Post(':id')
+  @ApiOperation({ summary: 'Promote a user to admin' })
   @ApiParam({
     name: 'id',
     required: true,
@@ -25,7 +27,7 @@ export class PromoteAdminController {
   })
   @ApiResponse({
     status: 200,
-    description: 'A user has been successfully fetched',
+    description: 'Promoted',
     type: User,
   })
   @ApiResponse({
@@ -33,7 +35,10 @@ export class PromoteAdminController {
     description: 'A user with given id does not exist.',
   })
   @HttpCode(HttpStatus.OK)
-  async getOne(@Param('id') id: string, @Body() pw: string): Promise<User> {
-    return await this.useCase.execute(id, pw);
+  @UseInterceptors(TransformInterceptor)
+  async getOne(@Param('id') id: string, @Body() pw: string): Promise<any> {
+    const result = await this.useCase.execute(id, pw);
+
+    return { message: 'Promote successfully', result };
   }
 }
